dd5d590c131af3192ea26002119a75c41279a043
Gennadii <oracl.vpn@mail.ru>
2022-07-27 18:29:16 +1100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {CustomPreloadStrategy} from "./custom-preload-strategy";
 
 const routes: Routes = [
 
